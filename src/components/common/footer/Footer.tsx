097219f26aabfd5fb92a99ab345fde139cf2368c
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-05-22 20:59:19 -0300

    add hover class to footer text

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className=" py-5 flex justify-center items-center sm:text-lg gap-2">
-            <Link href="https://matiasnmolina.com" target='_blank' className='text-green-900 font-medium' >
+            <Link href="https://matiasnmolina.com" target='_blank' className='text-green-800 hover:text-green-950 font-medium' >
                 Matias Molina
             </Link>
             <span className='text-black'>
